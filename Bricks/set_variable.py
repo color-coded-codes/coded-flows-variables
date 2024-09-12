d2ae0a82b54a7971ea2d55a0ff0bf4215e99f54d
Amine Lemaizi <40530011+aminelemaizi@users.noreply.github.com>
2024-09-12 23:34:42 +0100

    base variables bricks

---
+++ Bricks/set_variable.py
@@ -0,0 +1,13 @@
+from coded_flows.types import Any
+
+
+coded_flows_metadata = {
+    "display_name": "Set Variable",
+    "description": "Set a variable value",
+    "type": "variable",
+    "icon": "code",
+}
+
+
+def set_variable(value: Any):
+    pass
